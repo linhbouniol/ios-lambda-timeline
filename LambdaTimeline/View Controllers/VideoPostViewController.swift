412ccf00429b7493ee1a2b66d19a2048f3c58eb8
Linh Bouniol <paulalinh89@gmail.com>
2018-10-17 19:33:14 -0700

    Add video recording

---
+++ LambdaTimeline/View Controllers/VideoPostViewController.swift
@@ -0,0 +1,153 @@
+//
+//  VideoPostViewController.swift
+//  LambdaTimeline
+//
+//  Created by Linh Bouniol on 10/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPostViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
+    
+    // MARK: - Properties
+    
+    var postController: PostController!
+    var post: Post?
+    
+    private var captureSession: AVCaptureSession!
+    private var recordOutput: AVCaptureMovieFileOutput!
+    private var lastRecordedURL: URL?
+    
+    // MARK: - Outlets/Actions
+    
+    @IBOutlet weak var cameraPreviewView: CameraPreviewView!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBAction func toggleRecord(_ sender: Any) {
+        if recordOutput.isRecording {
+            recordOutput.stopRecording()
+        } else {
+            recordOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    
+    @IBAction func createPost(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "New Video Post", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Title"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "Post", style: .default) { (_) in
+            guard let title = alert.textFields?[0].text, title.count > 0 else {
+                self.presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
+                return
+            }
+            
+            // get the data out of video url
+            guard let url = self.lastRecordedURL, let data = try? Data(contentsOf: url) else { return }
+            
+            self.postController.createPost(with: title, ofType: .video, mediaData: data, ratio: 9.0/16.0) { (success) in
+                guard success else {
+                    DispatchQueue.main.async {
+                        self.presentInformationalAlertController(title: "Error", message: "Unable to create post. Try again.")
+                    }
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+        })
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - View Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCapture()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
+
+    // MARK: - Methods
+    
+    private func setupCapture() {
+        let captureSession = AVCaptureSession()
+        let device = bestCamera()
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: device), captureSession.canAddInput(videoDeviceInput) else { fatalError() }
+        
+        captureSession.addInput(videoDeviceInput)
+        
+        let fileOutput = AVCaptureMovieFileOutput() // creates a movie file
+        guard captureSession.canAddOutput(fileOutput) else { fatalError() } // make sure we can add it to captureSession
+        captureSession.addOutput(fileOutput)
+        recordOutput = fileOutput
+        
+        captureSession.sessionPreset = .hd1920x1080 // easier to filter with core image and process
+        captureSession.commitConfiguration() // save all this stuff and actually set it up
+        
+        self.captureSession = captureSession    // starts off not running, so need to start it in viewWillAppear()
+        cameraPreviewView.videoPreviewLayer.session = captureSession  // display the capture
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        // can allow user to choose different types of camera: dual, front, back
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            return device
+        } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) { // one camera
+            return device
+        } else {
+            fatalError("Missing expected back camera device")
+        }
+    }
+    
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("mov")
+    }
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        
+        let recordingButtonImageName = recordOutput.isRecording ? "Stop" : "Record"
+        recordButton.setImage(UIImage(named: recordingButtonImageName)!, for: .normal)
+    }
+    
+    // MARK: - AVCaptureFileOutputRecordingDelegate
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        DispatchQueue.main.async {
+            self.updateViews()
+            
+            self.lastRecordedURL = outputFileURL
+        }
+    }
+}
